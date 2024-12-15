5863a9f6486bcec7f79157da9526857100a7dce1
JasperVandenberghen <jasper.vandenberghen@gmail.com>
2024-12-15 15:05:33 +0100

    fix: typo

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,13 +1,11 @@
 import Box from '@mui/material/Box';
 
-import MainGrid from './MainGrid';
-
 const Home = () => {
   return (
     <>
       <Box sx={{ display: 'flex' }}>
         <Box component="main">
-            <MainGrid />
+            
         </Box>
       </Box>
     </>
